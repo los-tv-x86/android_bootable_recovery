454d58a45c2bea443dccb9f3a10cacefd7874608
hmtheboy154 <buingoc67@gmail.com>
2024-03-17 21:09:22 +0000

    Restore ApplyFromSdcard, with a twist
    Instead of ensuring /sdcard or /data/media/0 is mounted, ensure
    /data is mounted if /data is a partition or data.img
    
    If /data is a folder then just skip the mount check
    
    Change-Id: I832579e2aef45c492f2e83575783907d57952a85
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- recovery.cpp
+++ recovery.cpp
@@ -268,6 +268,7 @@ static InstallResult apply_update_menu(Device* device, Device::BuiltinAction* re
 
     items.clear();
     items.push_back("Apply from ADB");
+    items.push_back("Choose from internal storage (/sdcard)");
 
     if (InitializeVirtiofs()) {
       non_storage_items++;
@@ -300,6 +301,8 @@ static InstallResult apply_update_menu(Device* device, Device::BuiltinAction* re
 
     if (chosen == item_sideload) {
       status = ApplyFromAdb(device, false /* rescue_mode */, reboot_action);
+    } else if (chosen == 1) {
+      status = ApplyFromSdcard(device);
     } else if (chosen == item_virtiofs && InitializeVirtiofs()) {
       status = ApplyFromVirtiofs(device);
     } else {
